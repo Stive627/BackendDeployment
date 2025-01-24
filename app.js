dbff7ea4c61386559aa1db3451895a1bb05c48a3
Stive627 <fossistive627@gmail.com>
2025-01-25 01:26:55 +0530

    25-01-2025

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT
-app.get('/', (req, res)=>{res.status(200).send('you just completed the cloud training for TNexon👌 ')})
+app.get('/', (req, res)=>{res.status(200).send('We just completed the cloud training for TNexon👌 ')})
 app.listen(port, ()=>console.log(`The server is running at http://localhost:${port}`))
